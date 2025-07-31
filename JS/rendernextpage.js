c559e2d524838f07cd60f9f95e702a16230393f3
khan-aafiya <khanaafiya4747@gmail.com>
2025-07-31 08:42:26 +0530

    in rendernextpage.js i was remove error

--- JS/rendernextpage.js
+++ JS/rendernextpage.js
@@ -2,7 +2,7 @@ let nextPageBtns = document.querySelectorAll('.nextPage');
 
 nextPageBtns.forEach(nextPageBtn => {
     nextPageBtn.addEventListener('click', () => {
-        let card = nextPage.parentElement;
+        let card = nextPageBtn.parentElement;
         let h2 = card.childNodes[3];
         let categoryName = h2.innerText;
         window.location.href = `/Pages/${categoryName}.html`; 
