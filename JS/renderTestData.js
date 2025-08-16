10b6d16a7c4be4f13225b75ba4c282aede2b76bf
khan-aafiya <khanaafiya4747@gmail.com>
2025-08-16 22:29:48 +0530

    mai ne lock ko unlock kr ne ka logic add kr di rendertestData.js me, lock will be unlocked at the time 10:22 PM (this time is temporary) and also add logic for checkin day, means checked days can be track by localStorage as well as unlock card

--- JS/renderTestData.js
+++ JS/renderTestData.js
@@ -8,6 +8,16 @@ const dirName = ((window.location.pathname).split("_")[0].split("/")[2]) + "Data
 // Base filename without extension (used for both main and test JSON files)
 const fileNameBase = ((window.location.pathname).split("_")[1].split("/")[1].split("%")[0]);
 
+const container = document.querySelector(".container");
+
+// days checked
+let checkedDays = [];
+
+// unlocked
+let unlockedCard = [
+    { cardIdx: 1, day: 1 },
+];
+
 // Collect all day navigation buttons (must be declared in your HTML)
 // const nextPageBtns = document.querySelectorAll(".day-btn");
 
@@ -21,6 +31,7 @@ nextPageBtns.forEach(btn => {
 
         // If Day 1 → Skip test and go directly to Day page
         if (day === 1) {
+            checkin(fileName, day);
             goToDayPage(day, fileName);
             return;
         }
@@ -29,10 +40,113 @@ nextPageBtns.forEach(btn => {
         const breakPoint = dirName.length - 4;
         const testDir = dirName.slice(0, breakPoint) + "Test" + dirName.slice(breakPoint);
 
+        // if (isUnlocked(day)) {
         // Fetch the corresponding test file
         fetchData(`/Pages/${ testDir }/${ fileName }`, day);
+        // }
     });
 });
+
+// This function select all lockedDiv. check it's need to be unlock as time condition(12:00am), If yes then this fun will be add unlock the card and return true otherwise return false.
+
+
+function isUnlocked() {
+    // this card will be unlocked
+    const lockCard = document.querySelectorAll(".lock")[0];
+    const card = lockCard.parentNode;
+
+    const [key, day] = card.childNodes[1].childNodes[3].childNodes[5].childNodes[1].innerText.split(" ");
+
+    let cardIdx = idxOfCard(card);
+    let image = lockCard.children[0];
+
+    const standardTime = '10:22 PM';
+    let crrTime = currentTime();
+
+    if (standardTime == crrTime) {
+        image.src = '/assets/unlock-svgrepo-com.svg';
+        image.alt = "unlock";
+        image.addEventListener("click", () => {
+            lockCard.classList.add("unlock");
+            lockCard.classList.remove("lock");
+        });
+        unlockedCard.push({ cardIdx, day });
+        unlockedCardStoreInLocalStorage();
+    }
+}
+
+function changeLockIcon(card) {
+    let image = (card.children[1]).children[0];
+    image.src = '/assets/unlock-svgrepo-com.svg';
+    image.alt = "unlock";
+    image.addEventListener("click", () => {
+        const lockDiv = card.childNodes[3];
+        lockDiv.classList.add("unlock");
+        lockDiv.classList.remove("lock");
+    });
+}
+
+function idxOfCard(crd) {
+    let index = 0;
+    container.childNodes.forEach((card, idx) => {
+        if (card == crd) {
+            index = idx;
+        }
+    });
+    return index;
+}
+
+// check previous day is checkin
+function prevDayIsCheckin(fileName, currday) {
+    let prevDay = currday - 1;
+    const checkedDays = localStorage.getItem(`${ fileName } Checkin`);
+    if (checkedDays) {
+        try {
+            const parsed = JSON.parse(checkedDays);
+            // Merge all topics, including new user-created ones
+            parsed.forEach((obj) => {
+                if (obj.day == prevDay) {
+                    return true;
+                }
+            });
+        } catch (e) {
+            console.error("Failed to parse saved cheched days", e);
+        }
+        return false;
+    }
+}
+
+function currentTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    // 12-hours format
+    hours = hours % 12;
+    hours = hours ? hours : 12; // 0 -> 12
+
+    return `${ hours }:${ minutes } ${ ampm }`;
+}
+
+// remove lock from checked card
+function checkedCards() {
+    const lockedCards = document.querySelectorAll(".lock");
+
+    lockedCards.forEach(lockedCard => {
+        const card = lockedCard.parentNode;
+        const [key, day] = card.childNodes[1].childNodes[3].childNodes[5].childNodes[1].innerText.split(" ");
+
+        checkedDays.forEach(obj => {
+            if (obj.day == day) {
+                lockedCard.classList.add("unlock");
+                lockedCard.classList.remove("lock");
+            }
+        })
+
+    })
+}
 
 // -------------------
 // Go to Day.html Page
@@ -52,6 +166,7 @@ function fetchData(filePath, day) {
 
             // If test not found or empty → Go directly to Day page
             if (!currentDay || !Array.isArray(currentDay.test) || currentDay.test.length === 0) {
+                checkin(`${ fileNameBase }.json`, day);
                 goToDayPage(day, `${ fileNameBase }.json`);
                 return;
             }
@@ -180,7 +295,10 @@ function handleSubmit(currentDay, form, submitBtn) {
     form.querySelector(".score").textContent = `Score: ${ score }/${ currentDay.test.length } - ${ score >= passMark ? "✅ Passed" : "❌ Failed" }`;
 
     submitBtn.textContent = "Next Day";
-    submitBtn.onclick = () => goToDayPage(parseInt(currentDay.day), `${ fileNameBase }.json`);
+    submitBtn.onclick = () => {
+        checkin(`${ fileNameBase }.json`, currentDay.day);
+        goToDayPage(parseInt(currentDay.day), `${ fileNameBase }.json`);
+    };
 }
 
 // ------------------------
@@ -197,3 +315,90 @@ function getCorrectKey(q) {
     if (q.answer && typeof q.answer === "object") return Object.keys(q.answer)[0].toUpperCase();
     return null;
 }
+
+
+
+
+// Track day checkin and store in localstorage
+function checkin(fileName, day) {
+    checkedDays.push({ "day": day, "check": true });
+    localStorage.setItem(`${ fileName } Checkin`, JSON.stringify(checkedDays));
+}
+
+// unlock the card
+function unlockedTheCard() {
+    let arrIdx = 0;
+    container.childNodes.forEach((card, cardIdx) => {
+        if (cardIdx % 2 != 0) {
+            if (arrIdx < unlockedCard.length) {
+                if (cardIdx == unlockedCard[arrIdx].cardIdx && arrIdx != 0) {
+                    changeLockIcon(card);
+                }
+            }
+            arrIdx++;
+        }
+    })
+}
+
+// add unlocked Card in localstorage
+function unlockedCardStoreInLocalStorage() {
+    localStorage.setItem(`${ fileNameBase }.json Unlocked`, JSON.stringify(unlockedCard));
+}
+
+// load unlocked cards
+function loadUnlockedCard() {
+    const unlockedCards = localStorage.getItem(`${ fileNameBase }.json Unlocked`);
+    if (unlockedCards) {
+        try {
+            const parsed = JSON.parse(unlockedCards);
+
+            parsed.forEach((obj) => {
+                unlockedCard.push(obj);
+                removeDuplicates();
+            })
+        } catch (e) {
+            console.error("Failed to parse saved cheched days", e);
+        }
+        return false;
+    }
+}
+
+// remove duplicates from the unlockedCard Arr
+function removeDuplicates() {
+    let prev = unlockedCard[0];
+    let newArr = [prev];
+    let j = 1;
+    for (let i = 1; i <= unlockedCard.length - 1; i++) {
+        let cond = prev.cardIdx != unlockedCard[i].cardIdx;
+        if (cond) {
+            newArr[j] = unlockedCard[i];
+            prev = unlockedCard[i];
+            j++;
+        }
+    }
+    unlockedCard = newArr;
+}
+
+// load checkin days
+function loadCheckinDays() {
+    const checkedDaysls = localStorage.getItem(`${ fileNameBase }.json Checkin`);
+    if (checkedDaysls) {
+        try {
+            const parsed = JSON.parse(checkedDaysls);
+            parsed.forEach((obj) => {
+                if (!checkedDays.includes(obj)) {
+                    checkedDays.push(obj);
+                }
+            });
+        } catch (e) {
+            console.error("Failed to parse saved cheched days", e);
+        }
+    }
+}
+
+loadUnlockedCard();
+loadCheckinDays();
+unlockedTheCard();
+checkedCards();
+removeDuplicates();
+isUnlocked();
